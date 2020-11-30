c64afbfc2c36de8dbbaf6d159bc790e0627b6e34
gyuta <kuroshiba0408@gmail.com>
2020-11-30 22:07:06 +0900

    不要ファイルの削除や firebase の設定など

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBaulUbuSc8UlDbqU41n5kl-KBKYRyTkIE",
+  authDomain: "one-time-map.firebaseapp.com",
+  databaseURL: "https://one-time-map.firebaseio.com",
+  projectId: "one-time-map",
+  storageBucket: "one-time-map.appspot.com",
+  messagingSenderId: "1029387231473",
+  appId: "1:1029387231473:web:9e3b4e365438d711438950",
+  measurementId: "G-BVV21DPSLC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 Vue.config.productionTip = false
+
+Vue.prototype.$firebase = firebase
 
 new Vue({
   router,
---
+++ src/views/LP.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="button" @click='create'>マップを作成</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LP',
+  methods: {
+    create() {
+      // this.$firebase.firestore()
+      //   .collection('map') .....
+      //  みたいな感じでmapコレクション配下にドキュメントを作成して
+      // this.$router.push({name: 'map', params: {id: hogehoge}})
+      // みたいな感じでmapページに作成したドキュメントのidを渡す
+
+      // もしくは適当なライブラリを使って一意なidを生成してもよいかも　(uuidなど)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
